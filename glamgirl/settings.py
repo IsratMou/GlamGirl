43a4a5799411b28fa6d8e71313db78032a6a3384
IsratMou <moutherobogirl@gmail.com>
2025-12-01 00:24:31 +0600

    Products app with Category and Product models

--- glamgirl/settings.py
+++ glamgirl/settings.py
@@ -37,6 +37,12 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
+    # Third party apps
+    'rest_framework',
+
+    # My apps
+    'products',
 ]
 
 MIDDLEWARE = [
@@ -120,3 +126,8 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+
+# Media files (Product images)
+MEDIA_URL = '/media/'
+MEDIA_ROOT = BASE_DIR / 'media'
